dd7cc5cd38c0b31f19f19783838636d75635af6b
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2024-07-13 21:46:30 +0200

    Added postgreSQL database

--- str_to_vid/str_to_vid/settings.py
+++ str_to_vid/str_to_vid/settings.py
@@ -74,7 +74,14 @@ WSGI_APPLICATION = 'str_to_vid.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -85,6 +92,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
