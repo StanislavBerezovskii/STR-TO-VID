27dbd8d5cec7734f1bb17fb0da139528f658d47e
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2024-07-13 22:27:12 +0200

    Added postgreSQL database

--- str_to_vid/str_to_vid/settings.py
+++ str_to_vid/str_to_vid/settings.py
@@ -81,6 +81,19 @@ DATABASES = {
     }
 }
 
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'videodb',
+        'USER': 'django_admin',
+        'PASSWORD': 'django_pass',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+"""
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
