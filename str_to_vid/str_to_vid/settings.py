5ac1bb33a81f1f06a19dbadced451b4ed5aaa3ee
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2024-07-14 17:29:50 +0200

    Tried containerizing, failing because of libraries: PySide6==6.7.2 PySide6_Addons==6.7.2 PySide6_Essentials==6.7.2

--- str_to_vid/str_to_vid/settings.py
+++ str_to_vid/str_to_vid/settings.py
@@ -76,6 +76,14 @@ WSGI_APPLICATION = 'str_to_vid.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+"""
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'videodb',
         'USER': 'django_admin',
@@ -84,7 +92,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
